41c36d927cca22c51f12a2814fce988cd1a75baa
DonatoMoon <prvt.wrk.ndrsvchnk@gmail.com>
2023-09-17 22:26:53 +0300

    LR1_Fin

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Головний клас програми для обчислення суми перших N чисел Фібоначчі.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введіть номер числа Фібоначчі: ");
+        int n;
+        do {
+            n = in.nextInt();
+            if (n < 0)
+                System.out.print("\nВведено від'ємний номер числа Фібоначчі, введіть номер заново: ");
+        } while (n < 0);
+
+        long sum = FibNum.sumFirstNNumbers(n);
+        System.out.println("Сума перших " + n + " чисел Фібоначчі дорівнює: " + sum);
     }
 }
+
+
