1218de7429ee1ae90952c60f992c29be880fca88
DonatoMoon <prvt.wrk.ndrsvchnk@gmail.com>
2023-11-01 17:43:05 +0200

    switchzw

--- src/command/RemoveApplianceCommand.java
+++
@@ -1,8 +0,0 @@
-package command;
-
-public class RemoveApplianceCommand implements Command {
-    @Override
-    public void execute() {
-        System.out.println("Функціональність видалення приладу ще не реалізована.");
-    }
-}
